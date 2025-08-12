990ee73ad7402f3111470ac062313dc6ecdcdde1
Bruno Jurakić <178019478+brunojurakic@users.noreply.github.com>
2025-08-12 22:41:11 +0200

    add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,14 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/pages/LoginPage';
 
 const App = () => {
-  const [data, setData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/test`)
-      .then((res) => {
-        if (!res.ok) throw new Error("Failed to fetch");
-        return res.json();
-      })
-      .then(setData)
-      .catch((e) => setError(e.message))
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
-    <div>
-      <div>
-        {loading && <p>Loading...</p>}
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {data.length > 0 && (
-          <ul>
-            {data.map((item) => (
-              <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<div>App</div>} />
+      </Routes>
+    </Router>
   );
 }
 
